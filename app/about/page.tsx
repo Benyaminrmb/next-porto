80624a31eb7a5767aba8c745052088be4cf5ef32
Benyamin Rmb <benyaminrmb@gmail.com>
2025-01-06 21:26:25 +0330

    update title.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
+import type {Metadata} from 'next'
+
+export const metadata: Metadata = {
+  title: 'About Benyamin Bolhassani',
+  description: '...',
+}
 export default function About() {
   return <>awdawd</>
 }
